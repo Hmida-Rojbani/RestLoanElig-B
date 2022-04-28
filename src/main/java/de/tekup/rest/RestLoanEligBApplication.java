2202fe708457ee7343acb50e981332d425f95102
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2022-04-28 10:56:26 +0100

    add Documentation

--- src/main/java/de/tekup/rest/RestLoanEligBApplication.java
+++ src/main/java/de/tekup/rest/RestLoanEligBApplication.java
@@ -3,7 +3,13 @@ package de.tekup.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Loan Indicator Service", description = "To check the eligibility of client to have a credit",
+contact = @Contact(name = "Hmida Rojbani",email = "hmida.rojbani@tek-up.tn"),version = "1.0"))
 public class RestLoanEligBApplication {
 
 	public static void main(String[] args) {
--- src/main/java/de/tekup/rest/ctrl/LoanCtrl.java
+++ src/main/java/de/tekup/rest/ctrl/LoanCtrl.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 import de.tekup.rest.models.CustomerRequest;
 import de.tekup.rest.models.WsResponse;
 import de.tekup.rest.services.LoanService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.AllArgsConstructor;
 
 @RestController
@@ -17,10 +19,15 @@ public class LoanCtrl {
 	
 	private LoanService service;
 	
+	@Operation(summary = "checking client status",operationId = "Check Client",
+			responses = {
+					@ApiResponse(responseCode = "200", description = "Return Client Status"),
+					@ApiResponse(responseCode = "400", description = "Not yet implimented")
+			})
 	@PostMapping(path="/check/client/status",
 			consumes = {MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE},
 			produces = {MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE})
-	public WsResponse getStaus(@RequestBody CustomerRequest request) {
+	public WsResponse getStatus(@RequestBody CustomerRequest request) {
 		return service.getLoanStatus(request);
 	}
 
